b66527f8746cf7f7d95b4d5cc0421176e66ed4df
Oni <oni-mail@mail.ru>
2023-06-09 19:10:26 +0300

    sem_1_first

---
+++ Sem_1/t1_sem1.java
@@ -0,0 +1,19 @@
+// Написать программу, которая запросит пользователя ввести
+// <Имя> в консоли.
+// Получит введенную строку и выведет в консоль сообщение
+// “Привет, <Имя>!”
+
+package Sem_1;
+
+public class t1_sem1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("dhgdlkgh");
+
+        System.out.println("sdfsfsfsdfs");
+
+
+
+    }
+    
+    
+}
---
+++ Sem_1/t9_sem1.java
@@ -0,0 +1,12 @@
+// Записать в файл следующие данные:
+// Name Surname Age
+// Kate Smith 20
+// Paul Green 25
+// Mike Black 23 
+
+
+package Sem_1;
+
+public class t9_sem1 {
+    
+}
