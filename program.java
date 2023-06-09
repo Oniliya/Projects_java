b6591718a65f4bca7a20d33410f1d0038bf9473c
Oni <oni-mail@mail.ru>
2023-06-09 18:20:37 +0300

    first commit

---
+++ program.java
@@ -0,0 +1,18 @@
+
+public class program {
+    public static void main(String[] args) {
+        System.out.println("dkjghdkfjhjfg");
+        System.out.println("dfgdfgdg");
+        
+        String s = "dkljf";
+        System.out.println(s);
+        
+        int a = 123;
+        a = a-- - --a;
+        System.out.println(a);
+
+        int b = 123;
+        b = --b - b--;
+        System.out.println(b);
+    }
+}
